e3e4780380b5168d37b57b794d4384c0cd830353
Anubhav Mishra <anubhavmishra2001@gmail.com>
2021-02-11 20:48:34 +0530

    modified:   input-module.c 	Identifies constants in equations.

--- input-module.c
+++ input-module.c
@@ -3,4 +3,32 @@
 This file contains functions to facilitate input of mathematical equations.
 
 */
-
+#include<stdio.h>
+#include<string.h>
+#define EQ_LENGTH 32
+void getEq(int n); // Takes number of unknows as input
+// driver function
+int main(){
+	getEq(3);
+	return 0;
+}
+void getEq(int n){
+	char eq[EQ_LENGTH];
+	float matrix[n][n+1];
+	int i, j, k;
+	for(i=0;i<n;i++){
+		fgets(eq, EQ_LENGTH, stdin);
+		// now extract info from eq
+		for(j=0;j<strlen(eq);j++){
+			if(eq[j]==32){
+				continue;
+			}
+			else if((eq[j]>=48 && eq[j]<=57) || eq[j]=='.' || eq[j]=='/'){
+				printf("%c", eq[j]);
+			}
+			else if(eq[j]=='+' || eq[j]=='-' || eq[j]=='='){
+				printf(" ");
+			}
+		}
+	}
+}
